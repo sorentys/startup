19927b1a8fb8a9b634c999bcaf5835ce1f8a6374
Ty Sorensen <sorentys@gmail.com>
2023-06-15 20:11:50 -0600

    cleaning startup-react folder

--- startup-react/src/meeting_view/meeting_view.jsx
+++ startup-react/src/meeting_view/meeting_view.jsx
@@ -1,119 +1,26 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import Accordion from 'react-bootstrap/Accordion';
+
 
 export function MeetingView() {
   //grab id from url
-
-  const [load_event, setEvent] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        // Fetch data from the database using an API or library
-        const response = await fetch('/api/event' + id);
-        const load_event = await response.json();
-        
-        // Update the state with the fetched data
-        setEvent(load_event);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
-    <main>
-      <div className="accordion-holder">
-        <div className="accordion" id="accordion_template">
-
-          <div className="accordion-item">
-            <h2 className="accordion-header" id="headingOne">
-              <button id="button_description" className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                {load_event.name}
-              </button>
-            </h2>
-            <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordion_template">
-              <div className="accordion-body">
-                <textarea className="accordion_textarea" id="event-description" placeholder="Please include your event description here">{load_event.description}</textarea>
-              </div>
-            </div>
-          </div>
-
-          <div className="accordion-item">
-            <h2 className="accordion-header" id="headingTwo">
-              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                Attendees
-              </button>
-            </h2>
-            <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordion_template">
-              <div className="input-and-submit">
-                <input type="text" id="attendee-name" className="form-field" placeholder="Attendee"/>
-                <button type="submit" className="btn btn-primary">Add</button>
-              </div>
-              <div className="accordion-body">
-                <ul style="list-style: circle"  id="attendee_list">
-                </ul>
-              </div>
-            </div>
-          </div>
-          
-          <div className="accordion-item">
-            <h2 className="accordion-header" id="headingThree">
-              <button id="button_time_location" className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Time and Location
-              </button>
-            </h2>
-            <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordion_template">
-              <div className="accordion-body">
-                <textarea className="accordion_textarea" id="event-time-location" placeholder="Please include your event time and location here">{load_event.time_and_location}</textarea>
-              </div>
-            </div>
-          </div>
-
-          <div className="accordion-item">
-            <h2 className="accordion-header" id="headingFour">
-              <button id="button_chat" className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                Live Event Chat
-              </button>
-            </h2>
-            <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingFour" data-bs-parent="#accordion_template">
-              <div className="input-and-submit">
-                <fieldset id="name-controls" style="margin-bottom: 2%">
-                  <h6 className="event-paragraph">Name</h6>
-                  <input id="my-name" type="text" />
-                </fieldset>
-                <fieldset id="chat-controls" disabled>
-                  <h6 className="event-paragraph">Comments</h6>
-                  <input id="new-msg" type="text" />
-                  <button className="btn btn-primary">Send</button>
-                </fieldset>
-              </div>
-              <div className="accordion-body">
-                <div id="chat-text"></div>
-              </div>
-            </div>
-          </div>
-
-        </div>
+    <div>
+        <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey="0">
+                <Accordion.Header>Accordion Item #1</Accordion.Header>
+                <Accordion.Body>
+                    First Item
+                </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+                <Accordion.Header>Accordion Item #2</Accordion.Header>
+                <Accordion.Body>
+                    Second Item
+                </Accordion.Body>
+            </Accordion.Item>
+        </Accordion>
       </div>
-
-      <div className="image-container">
-        <img src="https://brightspotcdn.byu.edu/dims4/default/4284914/2147483647/strip/true/crop/1519x1139+206+0/resize/400x300!/quality/90/?url=https%3A%2F%2Fbrigham-young-brightspot.s3.amazonaws.com%2Ff2%2F2a%2F48bedd8247469c5d7d141a1f0ba1%2Fwsc-cougar-creations.png" alt="BYU Baseball" width="250" height="250"/>
-      </div>
-
-      <div className="save-container" id="save-container">
-        <h5>Please fill in the required fields and save your event information:</h5>
-        <button id="save_button" type="submit" className="btn btn-primary">Save</button>
-      </div>
-
-      <div className="return-container">
-        <h5>Return to events:</h5>
-        <a className="btn btn-outline-light" href="meetings.html">
-          Events
-        </a>
-      </div>
-    </main>
   );
 }
--- startup-react/src/meetings/meetings.jsx
+++ startup-react/src/meetings/meetings.jsx
@@ -1,47 +1,40 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Link, Route } from "react-router-dom";
-import { MeetingView } from "../meeting_view/meeting_view"
-
-
+import Accordion from 'react-bootstrap/Accordion';
+import { sendMessage } from "./chat_client"
 import './meetings.css';
 
 let events_list =[];
 
-//function to create new button for new meetings
-async function newMeeting() {
+export function Meetings() {
 
-  //grab meeting name
-  let event_name = document.getElementById("meeting_name").value;
+  //function to create new button for new meetings
+  async function newMeeting() {
 
-  //store event in server
-  const newEvent = {name: event_name, description: "", attendees: [], time_and_location: ""};
+    //store event in server
+    const newEvent = {name: event_name, description: "", attendees: [], time_and_location: ""};
 
-  try {
-      //post events
-      const response = await fetch('/api/event', {
-      method: 'POST',
-      headers: {'content-type': 'application/json'},
-      body: JSON.stringify(newEvent),
-      });
+    try {
+        //post events
+        const response = await fetch('/api/event', {
+        method: 'POST',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify(newEvent),
+        });
 
-      // Store events from service
-      const events = await response.json();
-      localStorage.setItem('events', JSON.stringify(events));
-  } 
-  catch {
-      // If there was an error then just track scores locally
-      localStorage.setItem("event_name" ,document.getElementById("meeting_name").value);
+        // Store events from service
+        const events = await response.json();
+        localStorage.setItem('events', JSON.stringify(events));
+    } 
+    catch {
+        // If there was an error then just track scores locally
+        console.log("error");
+    }
+
+    //add event to event list
+    events_list.push(newEvent);
   }
 
-  //add event to event list
-  events_list.push(newEvent);
-}
-
-function pressMeeting(event) {
-  localStorage.setItem('this_event', event);
-}
-
-export function Meetings() {
   const [events, setEvents] = React.useState([]);
 
   React.useEffect(() => {
@@ -59,13 +52,70 @@ export function Meetings() {
       });
   }, []);
 
+  function updateDescription(value) {
+    localStorage.setItem('description', value);
+  }
+
+  function updateTimeLocation(value) {
+    localStorage.setItem('time_location', value);
+  }
+
   if (events.length) {
     // Update the DOM with the events
     for (const [i, event] of  events.entries()) {
       events_list.push(
-        <li key={i} id='meeting_item'>
-          <button className='btn btn-outline-light' onClick={() => pressMeeting(event)} id={event._id}><Link to="/meeting_view">{event.name}</Link></button>
-        </li>
+        <div key={i}>
+          <Accordion defaultActiveKey="0">
+              <Accordion.Item eventKey="0">
+                  <Accordion.Header>Meeting: {event.name}</Accordion.Header>
+                  <Accordion.Body>
+                    <textarea 
+                      className="accordion_textarea" 
+                      id="event-description"
+                      placeholder="Please include your event description here"
+                      defaultValue={event.description}
+                      onChange={(e) => updateDescription(e.target.value)}>
+                    </textarea>
+                  </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="1">
+                  <Accordion.Header>Attendees</Accordion.Header>
+                  <Accordion.Body>
+                    <div className="input-and-submit">
+                      <input type="text" id="attendee-name" className="form-field" placeholder="Attendee"/>
+                      <button type="submit" className="btn btn-primary">Add</button>
+                    </div>
+                  </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="2">
+                  <Accordion.Header>Time and Location</Accordion.Header>
+                  <Accordion.Body>
+                  <textarea 
+                    className="accordion_textarea" 
+                    id="event-time-location" 
+                    placeholder="Please include your event time and location here"
+                    onChange={(e) => updateTimeLocation(e.target.value)}>
+                  </textarea>
+                  </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="3">
+                  <Accordion.Header>Live Chat</Accordion.Header>
+                  <Accordion.Body>
+                  <div className="input-and-submit">
+                    <fieldset id="name-controls">
+                      <h6 className="event-paragraph">Name</h6>
+                      <input id="my-name" type="text" />                      
+                    </fieldset>
+                    <fieldset id="chat-controls">
+                        <h6 className="event-paragraph">Comments</h6>
+                        <input id="new-msg" type="text" />
+                        <button className="btn btn-primary" onClick={sendMessage}>Send</button>
+                    </fieldset>
+                  </div>
+                  </Accordion.Body>
+              </Accordion.Item>
+          </Accordion>
+        </div>
       );
     }
 }
@@ -74,14 +124,20 @@ export function Meetings() {
     <main>
       <div className="input-container text-center">
         <h1>Please Create or Select an Event:</h1>
-          <input type="text" id="meeting_name" className="form-field" placeholder="Create a New Meeting"/>
-            <button type="submit" className="btn btn-primary" id="meeting_button" onClick={() => newMeeting()}>Submit</button>
-            <div className="meetings-container">
-              <ul className="meetings-list" id="meetings_list">
-                {events_list}
-              </ul>
-            </div>
+          <input 
+            type="text" 
+            id="meeting_name" 
+            className="form-field" 
+            placeholder="Create a New Meeting" 
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" className="btn btn-primary" id="meeting_button" onClick={() => newMeeting(event_name)}>Submit</button>
+        <div className="meetings-container">
+          <ul className="meetings-list" id="meetings_list">
+            {events_list}
+          </ul>
         </div>
-      </main>
+      </div>
+    </main>
   );
 }
